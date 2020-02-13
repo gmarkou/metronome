bdda0610b9f42a08878341fb763ba22c433a240f
George Markou <gmarkou@gmail.com>
2020-02-13 11:32:46 +0200

    change bpm

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import Header from './Header.js'
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bpm: 60
+    };
+
+    this.changeBpm = this.changeBpm.bind(this);
+  }
+
+  changeBpm(delta) {
+    let newBpm = Math.max(this.state.bpm + delta, 30);
+    newBpm = Math.min(newBpm, 400);
+    this.setState({bpm: newBpm});
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <div id="metronome" class="content">
           <div id="output">
-            <p id="bpm">60</p>
+            <p id="bpm">{this.state.bpm}</p>
           </div>
           <div class="center">
-            <button id="minus10">-10</button>
+            <button id="minus10" onClick={()=>this.changeBpm(-10)}>-10</button>
             <button id="startStop">Start</button>
-            <button id="plus10">+10</button>
+            <button id="plus10" onClick={()=>this.changeBpm(10)}>+10</button>
           </div>
         </div>
       </div>
